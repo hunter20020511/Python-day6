4adc84412ae0534fbfb83c9a63a01a26630e0325
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-27 21:21:21 +0000

    Solved Basics List VS Sets Problems

---
+++ Day6_Basics1_Set_Problems.py
@@ -0,0 +1,18 @@
+list=[2,4,3,3,1,3,2,1,3]
+
+set1=set(list) #Removing the duplicates from a list the basic way
+print(set1)
+
+a=[1,2,3,5,6,3]
+b=[21,4,1,3,5,66,6]
+common=set(a)&set(b) #Finding common elements in a list
+print(common)
+
+list1=[20,40,50,20,30]
+list2=[20,50,32,0,100]  
+common1=set(list1)&set(list2)  
+     
+if common1:   #Checking if common elemnets in alist exists and printing them
+    print("Common values exists which are: ", common1)
+else:
+    print("No common values exists in both lists")
