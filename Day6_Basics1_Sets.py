a28753cdb866753f910f570ca7ebdb5b23aa6e4f
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-27 19:52:50 +0000

    Learnt Sets in Python

--- Day6_Basics1_Sets.py
+++ Day6_Basics1_Sets.py
@@ -1,13 +1,13 @@
 set1={1,3,1,4,2,5,2,5,6,7,8}
-print(set1)
+print(set1) #Common way of defining set, the most common way as dictionary has only unique value
 
 set2=()
-print(set2)
+print(set2) #Defining empty set
 
 list=[1,2,3,4,3,4]
-set3=set(list)
+set3=set(list) #Converting to set from a list
 print(set3)
 
 tuple1=(1,2,4,6,6,7,4,4,6,10,55,204,210,203,204)
-set4=set(tuple1)
+set4=set(tuple1) #Converting to set from a tuple
 print(set4)
