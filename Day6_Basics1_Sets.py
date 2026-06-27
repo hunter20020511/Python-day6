e6c0353db31828c5620392544fc0ab4ac96322ba
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-27 19:47:46 +0000

    Learning Sets in Python

---
+++ Day6_Basics1_Sets.py
@@ -0,0 +1,13 @@
+set1={1,3,1,4,2,5,2,5,6,7,8}
+print(set1)
+
+set2=()
+print(set2)
+
+list=[1,2,3,4,3,4]
+set3=set(list)
+print(set3)
+
+tuple1=(1,2,4,6,6,7,4,4,6,10,55,204,210,203,204)
+set4=set(tuple1)
+print(set4)
