347cb496e3a2edea49aace0a64ceeb5ef9b807ee
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-29 01:40:57 +0530

    Solved mini challenge

---
+++ Day6_Basics2_MiniChallenge.py
@@ -0,0 +1,13 @@
+##Count how many times each word appears in a sentence##
+
+sentence="He is a nice guy but also a bad guy"
+lowe_sent=sentence.lower().split()  #Normalizing the given sentence and splitting it word by word
+
+counter={} #Initialising empty set
+
+for word in lowe_sent: #Looping over given sentece
+    if word in counter:
+        counter[word]+=1 #If word is found, then increasing the counter i.e empty set by 1
+    else:
+        counter[word]=1  #If word not found then simply not updating it
+print(counter)  #Printing the result
